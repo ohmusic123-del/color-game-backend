b5d07219b21558a9da72a53e996b4b78b35cdaf8
ohmusic123-del <ohmusic.123@gmail.com>
2026-01-01 20:10:41 +0530

    Reorder mobile field properties in userSchema

--- models/User.js
+++ models/User.js
@@ -3,16 +3,12 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   mobile: {
     type: String,
-    unique: true,
-    required: true
+    required: true,
+    unique: true
   },
   password: {
     type: String,
     required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
   }
 });
 
