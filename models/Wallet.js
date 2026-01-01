27b84f890170f41c4971f30167c62234de18dc16
ohmusic123-del <ohmusic.123@gmail.com>
2026-01-01 19:27:48 +0530

    Add userId field to wallet schema

--- models/Wallet.js
+++ models/Wallet.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const walletSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
   balance: {
     type: Number,
     default: 1000
