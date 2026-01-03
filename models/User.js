342c865ebf4405c275d376824a12b00f209d93f7
ohmusic123-del <ohmusic.123@gmail.com>
2026-01-03 12:54:38 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  mobile: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  wallet: { type: Number, default: 1000 }
+  mobile: { type: String, unique: true },
+  password: String,
+
+  wallet: { type: Number, default: 0 },
+
+  bonus: { type: Number, default: 100 },
+  bonusUsed: { type: Number, default: 0 },
+
+  deposited: { type: Boolean, default: false },
+  totalWagered: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model("User", userSchema);
