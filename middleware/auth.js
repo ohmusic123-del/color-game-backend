100c3c423d9b36b1c7e72e0a30220eb1f4494f2d
ohmusic123-del <ohmusic.123@gmail.com>
2026-01-03 12:55:45 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.userId;
+    req.userId = decoded.id;
     next();
   } catch {
     res.status(401).json({ message: "Invalid token" });
