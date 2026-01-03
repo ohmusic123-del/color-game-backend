5a8a41036f720777a0503ecba22aed4dd79efc12
ohmusic123-del <ohmusic.123@gmail.com>
2026-01-03 23:49:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const auth = require("./middleware/auth");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => res.send("BIGWIN backend running"));
 
