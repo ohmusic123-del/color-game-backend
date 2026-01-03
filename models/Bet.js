77303215b4cba48cc6a4fc5f7b0198ad9b128bc5
ohmusic123-del <ohmusic.123@gmail.com>
2026-01-03 12:55:06 +0530

    Update Bet.js

--- models/Bet.js
+++ models/Bet.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const betSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  userId: mongoose.Schema.Types.ObjectId,
   color: String,
   amount: Number,
+  roundId: Number,
   result: String,
   payout: Number
-}, { timestamps: true });
+});
 
 module.exports = mongoose.model("Bet", betSchema);
