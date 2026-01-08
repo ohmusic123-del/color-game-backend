8b19e6e1064f491648d97d1b1c7eba2202b39934
ohmusic123-del <ohmusic.123@gmail.com>
2026-01-08 20:29:07 +0530

    Create Deposit.js

---
+++ models/Deposit.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const depositSchema = new mongoose.Schema({
+  mobile: String,
+  amount: Number,
+  utr: String,
+  status: { type: String, default: "PENDING" }, // PENDING | APPROVED | REJECTED
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Deposit", depositSchema);
