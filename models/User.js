168f960b3bffdf2bbaea649e905d0217c537b194
ohmusic123-del <ohmusic.123@gmail.com>
2026-01-12 00:44:34 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,71 +1,22 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
+const UserSchema = new mongoose.Schema(
   {
     mobile: {
       type: String,
       required: true,
-      unique: true,
-      index: true
+      unique: true
     },
-
     password: {
       type: String,
       required: true
     },
-
-    /* ======================
-       WALLET
-    ====================== */
     wallet: {
       type: Number,
       default: 0
-    },
-
-    bonus: {
-      type: Number,
-      default: 100
-    },
-
-    totalWagered: {
-      type: Number,
-      default: 0
-    },
-
-    /* ======================
-       DEPOSIT INFO
-    ====================== */
-    deposited: {
-      type: Boolean,
-      default: false
-    },
-
-    depositAmount: {
-      type: Number,
-      default: 0
-    },
-
-    /* ======================
-       WITHDRAW DETAILS
-    ====================== */
-    withdrawMethod: {
-      type: String,
-      enum: ["upi", "bank", "usdt"],
-      default: null
-    },
-
-    withdrawDetails: {
-      upiId: String,
-
-      bankName: String,
-      accountHolder: String,
-      accountNumber: String,
-      ifsc: String,
-
-      usdtAddress: String
     }
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", UserSchema);
