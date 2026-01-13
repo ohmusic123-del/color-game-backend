d6bd3ed1dd3f0c9e56cf7bdc6591427a441cb3e5
ohmusic123-del <ohmusic.123@gmail.com>
2026-01-13 07:34:34 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -14,6 +14,35 @@ const UserSchema = new mongoose.Schema(
     wallet: {
       type: Number,
       default: 0
+    },
+    bonus: {
+      type: Number,
+      default: 0
+    },
+    deposited: {
+      type: Boolean,
+      default: false
+    },
+    depositAmount: {
+      type: Number,
+      default: 0
+    },
+    totalWagered: {
+      type: Number,
+      default: 0
+    },
+    withdrawMethod: {
+      type: String,
+      enum: ["upi", "bank", "usdt"],
+      default: "upi"
+    },
+    withdrawDetails: {
+      upiId: String,
+      bankName: String,
+      accountHolder: String,
+      accountNumber: String,
+      ifsc: String,
+      usdtAddress: String
     }
   },
   { timestamps: true }
