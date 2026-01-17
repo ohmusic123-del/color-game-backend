9bcb5056f36e8bfae38003c1f207b8d6bfd69cd8
ohmusic123-del <ohmusic.123@gmail.com>
2026-01-17 14:56:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const http = require("http");
-const connectDB = require("./db");
+const connectDB = require("./config/db");
 const app = require("./app");
 const { endRoundAndPayout, ensureActiveRound } = require("./services/roundService");
 
